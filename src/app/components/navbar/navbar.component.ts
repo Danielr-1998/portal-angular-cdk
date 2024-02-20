1da5f5cf6f759bd98475885acb5069e4809f9e0a
Danielr-1998 <daniel.riano@utp.edu.co>
2024-02-20 12:45:45 -0500

    modificados:     package-lock.json 	modificados:     package.json 	modificados:     src/app/app.component.css 	modificados:     src/app/components/navbar/navbar.component.html 	modificados:     src/app/components/navbar/navbar.component.ts 	modificados:     src/app/pages/boards/boards.component.html 	modificados:     src/app/pages/boards/boards.component.ts 	modificados:     src/styles.css

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBell,faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
  isOpen=false;
+ faBell=faBell;
+ 
 }
--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import {CdkAccordionModule} from '@angular/cdk/accordion';
 
 @Component({
   selector: 'app-boards',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,CdkAccordionModule],
   templateUrl: './boards.component.html'
 })
 export class BoardsComponent {

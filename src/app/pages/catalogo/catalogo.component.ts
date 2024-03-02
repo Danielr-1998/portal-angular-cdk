81749f57142311f550d516ff166a488a2a3837c4
Danielr-1998 <daniel.riano@utp.edu.co>
2024-03-01 21:09:39 -0500

    nuevos archivos: src/app/components/sidebar/sidebar.component.css 	nuevos archivos: src/app/components/sidebar/sidebar.component.html 	nuevos archivos: src/app/components/sidebar/sidebar.component.ts 	modificados:     src/app/pages/catalogo/catalogo.component.html 	modificados:     src/app/pages/catalogo/catalogo.component.ts 	modificados:     src/app/pages/footer/footer.component.html 	modificados:     src/app/pages/home/home.component.html 	nuevos archivos: src/assets/svg/login-svgrepo-com.svg

--- src/app/pages/catalogo/catalogo.component.ts
+++ src/app/pages/catalogo/catalogo.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FooterComponent } from '../footer/footer.component';
-
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 @Component({
   selector: 'app-catalogo',
   standalone: true,
-  imports: [NavbarComponent,FooterComponent],
+  imports: [NavbarComponent,FooterComponent,SidebarComponent],
   templateUrl: './catalogo.component.html',
   styleUrl: './catalogo.component.css'
 })
